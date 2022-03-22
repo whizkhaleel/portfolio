06e8c2ad59288e7b9b7ad8fbf7e40b4def1ba54a
whizkhaleel <itzmelilfreshgee@gmail.com>
2022-03-22 16:11:14 -0700

    Uploaded v1.0 Portfolio.

---
+++ js/configs.js
@@ -0,0 +1,45 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+const themeBtn = document.querySelector('.theme-btn');
+
+function pageTransition() {
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function () {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        });
+    }
+
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if(id) {
+            // Remove selected from the other buttons
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active');
+            });
+            e.target.classList.add('active');
+
+            //Hide other sections
+            sections.forEach((section) =>{
+                section.classList.remove('active');
+            });
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    });
+
+    themeBtn.addEventListener('click', () => {
+        let element = document.querySelector('body');
+        element.classList.toggle('light-mode');
+        let icon = document.querySelector('#theme-icon');
+        if(element.className == 'main-content light-mode') {
+            icon.classList.replace('fa-sun', 'fa-moon');
+        } else {
+            icon.classList.replace('fa-moon', 'fa-sun');
+        }
+    });
+}
+pageTransition();
